36f3454d2657610d018062dd70bbd4f7e7773427
張泰瑋(Chang Tai Wei) <davidtnfsh@gmail.com>
2025-09-10 19:51:57 +0900

    chroes: remove RiskAssessment

--- src/components/RiskAssessment/components/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Risk Assessment Sub-components - Main Export
- */
-
-export { KeyTakeaway } from "./KeyTakeaway";
-export { RiskMetricCard } from "./RiskMetricCard";
-export { DrawdownStatus } from "./DrawdownStatus";
-export { RiskRecommendations } from "./RiskRecommendations";
