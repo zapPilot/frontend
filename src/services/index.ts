34f61eccdc77bc0c0b330b37c7d621ccc6650ba1
張泰瑋(Chang Tai Wei) <davidtnfsh@gmail.com>
2025-12-26 14:51:22 +0900

    fixCI

--- src/services/index.ts
+++ src/services/index.ts
@@ -4,6 +4,5 @@ export * as balanceService from "./balanceService";
 export * as bundleService from "./bundleService";
 export * as chainService from "./chainService";
 export * as intentService from "./intentService";
-export * as priceService from "./priceService";
 export * as tokenService from "./tokenService";
 export * as transactionService from "./transactionService";
